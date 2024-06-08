3db57a8e43fe063de1110430dda77f8dbb632e92
João Pedro <joaopedron253@gmail.com>
2024-06-08 16:39:30 -0300

    adição do componente balance que mostra o resumo do saldo e dos gastos

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -2,21 +2,68 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-const Balance = () => {
+const Balance = ({saldo, gastos}) => {
     return (
         <View style={styles.container}>
-            <Text>Balance</Text>
+
+            <View style={styles.item}>
+                <Text style={styles.itemTitle}>Saldo</Text>
+                <View style={styles.content}>
+                    <Text style={styles.currencySymbol}>R$</Text>
+                    <Text style={styles.balance}>{saldo}</Text>
+                </View>
+            </View>
+
+            <View style={styles.item}>
+                <Text style={styles.itemTitle}>Gastos</Text>
+                <View style={styles.content}>
+                    <Text style={styles.currencySymbol}>R$</Text>
+                    <Text style={styles.expenses}>{gastos}</Text>
+                </View>
+            </View>
+
         </View>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#2c3e50',
+        backgroundColor: '#fff',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingStart: 18,
+        paddingEnd: 18,
+        marginTop: -24,
+        marginStart: 14,
+        marginEnd: 14,
+        borderRadius: 4,
+        paddingTop: 22,
+        paddingBottom: 22,
+        zIndex: 99
     },
+    item: {
+
+    },
+    itemTitle: {
+        fontSize: 20,
+        color: '#8d8a86',
+    },
+    content: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    currencySymbol: {
+        color: '#8d8a86',
+        marginRight: 6
+    },
+    balance: {
+        fontSize: 20,
+        color: '#2ecc71'
+    },
+    expenses: {
+        fontSize: 20,
+        color: '#e74c3c'
+    }
 });
 
 export default Balance;
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -4,11 +4,37 @@ import { StyleSheet, Text, View } from 'react-native';
 import Header from '../components/Header';
 import Balance from '../components/Balance';
 
+const list = [
+    {
+        id: 1,
+        label: 'Conta de Luz',
+        value: '300,90',
+        date: '17/05/2024',
+        type: 0 // despesas
+    },
+    {
+        id: 2,
+        label: 'Pix Cliente 1',
+        value: '50,00',
+        date: '17/05/2024',
+        type: 1 // receita
+    },
+    {
+        id: 3,
+        label: 'Salário',
+        value: '3500,00',
+        date: '18/05/2024',
+        type: 1 // receita
+    }
+]
+
 export default function Home() {
   return (
     <View style={styles.container}>
       <Header name="João Pedro" />
-      <Balance />
+      <Balance saldo="9.250,50" gastos="-527,00" />
+
+      <Text style={styles.title}>Últimas Movimentações</Text>
     </View>
   );
 }
@@ -16,6 +42,13 @@ export default function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fafafa',
   },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginLeft: 14,
+    marginRight: 14,
+    marginTop: 14
+  }
 });
