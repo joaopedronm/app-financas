84a50554011ac4cc52f5c2a89824b1a2717d7f4e
João Pedro <joaopedron253@gmail.com>
2024-06-10 09:45:23 -0300

    criação dos componentes de balanço e de ações

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,15 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 import Header from '../components/Header';
 import Balance from '../components/Balance';
+import Moviments from '../components/Moviments';
+import Actions from '../components/Actions';
 
 const list = [
     {
         id: 1,
         label: 'Conta de Luz',
         value: '300,90',
-        date: '17/05/2024',
+        date: '16/05/2024',
         type: 0 // despesas
     },
     {
@@ -34,7 +36,17 @@ export default function Home() {
       <Header name="João Pedro" />
       <Balance saldo="9.250,50" gastos="-527,00" />
 
+      <Actions />
       <Text style={styles.title}>Últimas Movimentações</Text>
+
+      <FlatList
+      style={styles.list}
+      data={list}
+      keyExtractor={(item) => String(item.id)}
+      showsVerticalScrollIndicator={false}
+      renderItem={({item}) => <Moviments data={item} /> }
+      
+      />
     </View>
   );
 }
@@ -47,8 +59,10 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 18,
     fontWeight: 'bold',
-    marginLeft: 14,
-    marginRight: 14,
-    marginTop: 14
+    margin: 14
+  },
+  list: {
+    marginStart: 14,
+    marginEnd: 14
   }
 });
